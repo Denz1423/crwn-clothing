1926f24f0ed7a167f8e70c98b2ce996172f636fa
davis <davisdjaja20@gmail.com>
2023-08-17 16:20:49 +1200

    first commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.style.scss";
+
+export default function Directory({ categories }) {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+}
